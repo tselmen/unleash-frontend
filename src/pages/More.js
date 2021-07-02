fcfc0827620be2155ebdc11f688d8aaa191cdfd1
Tselmen <tselmen@mn.data-artist.com>
2021-07-02 22:36:33 +0800

    added date time picker

--- src/pages/More.js
+++ src/pages/More.js
@@ -1,23 +1,47 @@
 import { Line } from 'react-chartjs-2';
+import { useState } from 'react';
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
 
 const state = {
-  labels: ['January', 'February', 'March', 'April', 'May'],
+  labels: ['00', '01', '02', '03', '04'],
   datasets: [
     {
-      label: 'Rainfall',
+      label: 'PPM',
       fill: false,
       lineTension: 0.5,
       backgroundColor: 'rgba(75,192,192,1)',
       borderColor: 'rgba(0,0,0,1)',
       borderWidth: 1,
-      data: [65, 59, 80, 81, 56],
+      data: [7, 8, 8, 9, 7],
     },
   ],
 };
 
 export default function Statistics() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
   return (
     <div>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Choose date"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+      </MuiPickersUtilsProvider>
       <Line
         data={state}
         options={{
